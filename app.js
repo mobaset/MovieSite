e37e158324cdfdf01ce29b06d92e5fd9b75c14bc
Mo <mo.baset>
2016-03-12 00:13:17 +0400

    adding and using express

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 /**
  * Created by Mo on 3/11/2016.
  */
-var http = require('http');
-var fs = require('fs');
+var express = require('express'), fs = require('fs');
+var app = express();
+var port = process.env.PORT || 8080; // a variable that stores a dynamic port
 
-var port = process.env.PORT || 8080;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type' : 'application/xhtml+xml'});
-    fs.createReadStream(__dirname + '/Views/MoviePage.xhtml').pipe(res);
+// a middleware to grap CSS and JS files
+app.use('/public', express.static(__dirname + '/public'));
 
-    //var html = fs.readFile(__dirname + '/views/MoviePage.xhtml');
-    //res.end(html);
-}).listen(port, function  () {
-    console.log('Express server listening on %d', port);
+// the get method to open the homepage when the url is called.
+app.get('/', function (req, res) {
+    fs.createReadStream(__dirname + '/Views/HomePage.xhtml').pipe(res);
 });
+// a get method to load movie page
+app.get('/MoviePage.xhtml', function (req, res) {
+    fs.createReadStream(__dirname + '/Views/MoviePage.xhtml').pipe(res);
+});
+
+app.listen(port);
